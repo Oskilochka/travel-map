e2ebd867c3b8688994db829f499272130cd5f55e
Julia Oskilko <yulia.osk14@gmail.com>
2023-11-09 08:26:35 +0100

    small fix

--- server/routes/marker.js
+++ server/routes/marker.js
@@ -4,12 +4,11 @@ const Marker = require("../models/Marker")
 
 router.post("/", async (req, res) => {
     const marker = new Marker(req.body)
+
     try {
         const saved = await marker.save();
-        debugger
         res.status(200).json(saved)
     } catch (e) {
-        debugger
         res.status(500).json(e)
     }
 })
