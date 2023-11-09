5700fa9264f291b06124bbc9efa219afa5532193
Julia Oskilko <yulia.osk14@gmail.com>
2023-11-09 07:26:05 +0100

    server changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
-const mongoose = require("mongoose")
+const mongoose = require("mongoose").default
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+    .then(() => console.log("Mongo DB"))
 
 app.listen(8080, () => console.log("Server 8080"))
